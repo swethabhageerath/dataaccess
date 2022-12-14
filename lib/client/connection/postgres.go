7cc79723c5e82804085b3f0ed72e7cf08868b284
swethabhageerath <swethabhageerath@gmail..com>
2022-12-13 20:56:03 -0500

    bug fix

--- lib/client/connection/postgres.go
+++ lib/client/connection/postgres.go
@@ -1,27 +1,32 @@
 package connection
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 
 	"github.com/swethabhageerath/dataaccess/lib/client/connection/config"
 	"github.com/swethabhageerath/logging/lib/constants"
 	"github.com/swethabhageerath/logging/lib/logger"
+	"github.com/swethabhageerath/utilities/lib/utilities/helpers"
 )
 
 type Postgres struct {
 	config config.PgConfig
 	logger logger.ILogger
+	env    helpers.IEnvironmentHelper
 }
 
-func NewPostgresConnection(config config.PgConfig, logger logger.ILogger) Postgres {
+func NewPostgresConnection(config config.PgConfig, logger logger.ILogger, env helpers.IEnvironmentHelper) Postgres {
 	return Postgres{
 		config: config,
 		logger: logger,
+		env:    env,
 	}
 }
 
-func (p Postgres) Connect() (*sql.DB, error) {
+func (p Postgres) Connect(ctx context.Context) (*sql.DB, error) {
+	p.config.Set(ctx, p.env, p.logger)
 	db, err := sql.Open("postgres", p.config.String())
 	if err != nil {
 		p.logger.AddAppName("dataaccess").AddMessage(fmt.Sprintf("Postgres.Connect() -> Open() -> %s", err.Error())).
