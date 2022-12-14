72e903a9b1b15f8cab44007503b0094e34f42f4d
swethabhageerath <swethabhageerath@gmail..com>
2022-12-13 20:51:13 -0500

    added i

--- lib/client/execute.go
+++ lib/client/execute.go
@@ -18,3 +18,11 @@ func NewExecute(db *sql.DB) Execute {
 func (e Execute) ExecuteContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
 	return e.db.ExecContext(ctx, query, args)
 }
+
+func (e Execute) QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row {
+	return e.db.QueryRowContext(ctx, query, args)
+}
+
+func (e Execute) QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
+	return e.db.QueryContext(ctx, query, args)
+}
